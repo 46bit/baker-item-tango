0a1a84d161a9c46e366df32dd9e2dc7ad112db3c
Michael Mokrysz <hi@46bit.com>
2014-01-11 12:55:19 +0000

    Added initial hunting mode to explore diagonals across the squares.

--- Players/bit.py
+++ Players/bit.py
@@ -40,8 +40,6 @@ class Player(base_player.BasePlayer):
     return True
 
   def deployPieces(self, pieces):
-    # shuffle pieces!
-
     for piece in pieces:
       # print "%d segment piece" % len(piece)
       while True:
@@ -66,8 +64,30 @@ class Player(base_player.BasePlayer):
     self.deployPieces(pieces)
 
     self._playedMoves = [[-1, -1]]
-    self._warMode = "hunt"
     self._possibleTargets = []
+    self._warMode = "hunt"
+
+    self._huntMode = "tiling"
+    self._diagonalMoves = []
+
+    # Create hunting moves for diagonals across each 6x6 square.
+    for i in range(0, 6):
+      self._diagonalMoves.append([i, i])
+      self._diagonalMoves.append([5 - i, 5 - i])
+
+      self._diagonalMoves.append([i + 6, i])
+      self._diagonalMoves.append([5 - i + 6, 5 - i])
+
+      self._diagonalMoves.append([i + 6, i + 6])
+      self._diagonalMoves.append([5 - i + 6, 5 - i + 6])
+
+    # Fill in the gaps between square diagonals a bit, quick improvement.
+    self._diagonalMoves.append([5, 3])
+    self._diagonalMoves.append([6, 2])
+    self._diagonalMoves.append([8, 5])
+    self._diagonalMoves.append([9, 6])
+
+    shuffle(self._diagonalMoves)
 
     return self._playerBoard
 
@@ -80,18 +100,9 @@ class Player(base_player.BasePlayer):
     """
 
     if self._warMode == "hunt":
-      move = [-1, -1]
-      while move in self._playedMoves:
-        move[0] = randint(0, 11)
-
-        if move[0] < 6:
-          move[1] = randint(0, 2) * 2
-        else:
-          move[1] = randint(0, 5) * 2
-        if move[0] % 2 == 1:
-          move[1] = move[1] + 1
+      move = self.chooseHuntMove()
     elif self._warMode == "sink":
-      move = self._possibleTargets.pop(0)
+      move = self.chooseSinkMove()
 
     # print move
     self._playedMoves.append(move)
@@ -99,6 +110,32 @@ class Player(base_player.BasePlayer):
     # Display move in row (letter) + col (number) grid reference
     # e.g. A3 is represented as 0,2
     return move[0], move[1]
+
+  def chooseHuntMove(self):
+    move = [-1, -1]
+
+    if self._huntMode == "diagonal":
+      while move in self._playedMoves:
+        if len(self._diagonalMoves) == 0:
+          self._huntMode = "tiling"
+          break
+        move = self._diagonalMoves.pop()
+
+    if self._huntMode == "tiling":
+      while move in self._playedMoves:
+        move[0] = randint(0, 11)
+        if move[0] < 6:
+          move[1] = randint(0, 2) * 2
+        else:
+          move[1] = randint(0, 5) * 2
+        if move[0] % 2 == 1:
+          move[1] = move[1] + 1
+
+    return move
+
+  def chooseSinkMove(self):
+    move = self._possibleTargets.pop(0)
+    return move
 
   def setOutcome(self, entry, row, col):
     """
